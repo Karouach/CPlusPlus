2e49b063067f71751273e640cfd9793a206fc5a6
karouach <guskarib@student.gu.se>
2015-09-26 11:38:05 +0200

    Function Signature

---
+++ funcSignature.cpp
@@ -0,0 +1,17 @@
+//
+// Created by karou on 26/09/2015.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+long volume (long a, long b, long c){
+    return a * b * c;
+}
+double volume(double r, int h){
+    return 3.14159 * r * r * h;
+}
+
+
