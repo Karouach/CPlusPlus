14ec339e538ef33b7ddb8175e10690a744d962a1
karouach <guskarib@student.gu.se>
2015-09-26 11:44:46 +0200

    Basics - Function Call

---
+++ func.cpp
@@ -0,0 +1,20 @@
+//
+// Created by karou on 26/09/2015.
+//
+
+#include <cstdio>
+#include "func.h"
+
+using namespace std;
+
+int main(int argc, char ** argv){
+    puts("this is main ()");
+    func();
+    return 0;
+}
+void func(){
+    puts("This is func");
+}
+
+
+
---
+++ func.h
@@ -0,0 +1,8 @@
+//
+// Created by karou on 26/09/2015.
+//
+
+#ifndef CPLUSPLUS_MAIN_CPP_H
+#define CPLUSPLUS_MAIN_CPP_H
+void func();
+#endif //CPLUSPLUS_MAIN_CPP_H
