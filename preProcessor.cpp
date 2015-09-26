6b6e2139ed74f25b73a34e44f2eb7a5a9c59c186
karouach <guskarib@student.gu.se>
2015-09-26 14:36:26 +0200

    Pre- Processor

---
+++ preProcessor.cpp
@@ -0,0 +1,17 @@
+//
+// Created by karou on 26/09/2015.
+//
+
+#include <cstdio>
+using namespace std;
+
+#define ONE 1
+#define HELLO_STRING "Hello World"
+
+int main( int argc, char ** argv ) {
+    printf("VALUE IS %d\n", ONE);
+    printf("STRING IS %s\n", HELLO_STRING);
+    return 0;
+}
+
+
