03c9175e725cfa751a032c566078b0487524bb42
karouach <guskarib@student.gu.se>
2015-09-26 15:17:37 +0200

    Macros Max

---
+++ testMacro.cpp
@@ -0,0 +1,16 @@
+//
+// Created by karou on 26/09/2015.
+//
+#include <cstdio>
+using namespace std;
+
+#define MAX(a , b ) ( a >  b ? a : b )
+
+
+int main( int argc, char ** argv )
+{
+    int ia = 5;
+    int ib = 7;
+    printf("value is %d\n", MAX (ia ,ib));
+    return 0;
+}
